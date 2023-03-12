6f2d907b4eef60841081175c6e3fe95123f68e28
Przemyslaw Fusik <przemyslaw.fusik@gmail.com>
2023-03-12 17:38:49 +0100

    better eq instances

--- src/main/scala/mycats/eq/EqInstances.scala
+++ src/main/scala/mycats/eq/EqInstances.scala
@@ -1,10 +1,12 @@
 package mycats.eq
 
 import cats.Eq
+import cats.implicits.catsSyntaxEq
 import mycats.Cat
 
 object EqInstances {
 
-  implicit val eqCat: Eq[Cat] = (x: Cat, y: Cat) => x == y
+  implicit val eqCat: Eq[Cat] =
+    (x: Cat, y: Cat) => x.age === y.age && x.name === y.name && x.color === y.color
 
 }
